4bcead4ab2b000adcf8036c1dbd17dde64602327
Cujba Tiberiu Stefan <123594218+CujbaTiberiu@users.noreply.github.com>
2023-03-29 17:50:16 +0200

    error key solution: (book,i) {book.asin + i}

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -10,7 +10,8 @@ import horror from '../data/horror.json'
 import romance from '../data/romance.json'
 import scifi from '../data/scifi.json'
 
-let allBooks = fantasy.concat(history,horror,romance,scifi)
+let allBooks =[] 
+allBooks =allBooks.concat(fantasy,history,horror,romance,scifi)
 console.log(allBooks)
 
 class AllTheBooks extends Component{
@@ -18,9 +19,9 @@ class AllTheBooks extends Component{
         return (
             <Container>
                 <Row>
-            { allBooks.map(book => { 
+            { allBooks.map((book,i) => { 
                 return(         
-                    <Col key={book.asin} xs={12} sm={6} md={4} lg={3} className="gy-2">
+                    <Col key={book.asin + i} xs={12} sm={6} md={4} lg={3} className="gy-2">
             <Card>
       <Card.Img variant="top" src={book.img} />
       <Card.Body className="d-flex flex-column justify-content-between">
